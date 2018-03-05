712d3ae7be874becca607c9051649dc62e89dd45
Aaron Middleton <aarontm9@gmail.com>
2018-03-04 21:02:00 -0700

    small header changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import { Navbar, NavItem, Icon } from 'react-materialize';
 class Header extends Component {
   render() {
     return (
-      <Navbar brand='Jazzy Tea' className='fixed' right>
+      <Navbar brand='Jazzy Tea' right>
         <NavItem href='/teas' ><Icon>kitchen</Icon></NavItem>
         <NavItem href='/about' ><Icon>sort</Icon></NavItem>
         <NavItem href='/login'><Icon>person</Icon></NavItem>
